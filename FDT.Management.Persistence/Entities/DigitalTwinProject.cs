f8a95c6b30015427e1991186bb8b0f8423b7f4c0
Farhad Hajnoruzi <farhad.hajnoruzi@gmail.com>
2023-06-11 10:16:11 +0200

    Added three needed fields to Project entity

--- FDT.Management.Persistence/Entities/DigitalTwinProject.cs
+++ FDT.Management.Persistence/Entities/DigitalTwinProject.cs
@@ -5,6 +5,9 @@ namespace FDT.Management.Persistence.Entities
     public class DigitalTwinProject : BaseEntity
     {
         public string? ProjectName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public int ProjectType { get; set; }
 
         public virtual ICollection<DigitalTwinEntity>? DigitalTwins { get; set; }
     }
---
+++ FDT.Management.Persistence/Migrations/20230611080502_AddedItemsToProject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FDT.Management.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedItemsToProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Projects",
+                nullable: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "Projects",
+                nullable: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProjectType",
+                table: "Projects",
+                nullable: false);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProjectType",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+              name: "DateModified",
+              table: "Projects");
+
+            migrationBuilder.DropColumn(
+              name: "DateCreated",
+              table: "Projects");
+        }
+    }
+}
