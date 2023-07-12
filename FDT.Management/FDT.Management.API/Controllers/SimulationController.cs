8b0386ca35a2b8b88ca5eac4fa7517f3d640bf79
Farhad Hajnoruzi <farhad.hajnoruzi@gmail.com>
2023-07-12 23:25:51 +0200

    simulation controller updated

--- FDT.Management/FDT.Management.API/Controllers/SimulationController.cs
+++ FDT.Management/FDT.Management.API/Controllers/SimulationController.cs
@@ -2,6 +2,7 @@
 using FDT.Management.Persistence.Entities;
 using FDT.WindTurbineLib.Simulation;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace FDT.Management.API.Controllers
 {
@@ -12,11 +13,15 @@ namespace FDT.Management.API.Controllers
 
         private readonly ILogger<SimulationController> _logger;
         private readonly IDigitalTwinRepository<DigitalTwinEntity> dtRepository;
+        private readonly IMemoryCache cache;
 
-        public SimulationController(ILogger<SimulationController> logger, IDigitalTwinRepository<DigitalTwinEntity> dtRepository)
+        public SimulationController(ILogger<SimulationController> logger, 
+            IDigitalTwinRepository<DigitalTwinEntity> dtRepository,
+            IMemoryCache cache)
         {
             _logger = logger;
             this.dtRepository = dtRepository;
+            this.cache = cache;
         }
 
         [HttpPost("{digitalTwinId}/{simulatorId}")]
@@ -43,6 +48,54 @@ namespace FDT.Management.API.Controllers
                 _logger.LogError(ex.Message);
                 return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while starting the simulation.");
             }
+        }
+
+        [HttpPost("continuous/start/{digitalTwinId}/{simulatorId}")]
+        public async Task<IActionResult> StartContinuous(int digitalTwinId, int simulatorId)
+        {
+            try
+            {
+                var digitalTwin = await dtRepository.GetById(digitalTwinId);
+                var rotorDiameter = double.Parse(digitalTwin.Properties
+                                        .FirstOrDefault(p => p.PropertyName == "RotorDiameter").PropertyValue);
+
+                // TODO: Load the simulator with the specified ID from your data source
+
+                var simulatedData = await WindTurbineSimpleSimulation.Simulate(rotorDiameter);
+                var simulationId = Guid.NewGuid();
+
+                cache.Set(simulationId.ToString(), simulatedData);
+
+                return Ok(new { message = "Simulation started successfully", simulationId });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while starting the simulation.");
+            }
+        }
+
+
+        [HttpGet("continuous/data/{simulationId}/{index}")]
+        public IActionResult GetContinuousData(Guid simulationId, int index)
+        {
+            if (!cache.TryGetValue(simulationId.ToString(), out var simulatedDataObj) || simulatedDataObj == null)
+            {
+                return BadRequest("Invalid simulation ID or missing simulated data.");
+            }
+
+            if (!(simulatedDataObj is IEnumerable<Tuple<double, double>> simulatedData))
+            {
+                return BadRequest("Invalid simulated data type.");
+            }
+
+            if (index < 0 || index >= simulatedData.Count())
+            {
+                return BadRequest("Invalid index.");
+            }
+
+            var simData = simulatedData.ToList()[index];
+            return Ok(new { windSpeed = simData.Item1, powerOutput = simData.Item2 });
         }
     }
 }
--- FDT.Management/FDT.Management.API/Program.cs
+++ FDT.Management/FDT.Management.API/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddControllers().AddNewtonsoftJson(options => {
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
 
 builder.Services.AddCors(options =>
 {
